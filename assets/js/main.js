a6dd02a0a2af4c530662700f7c11e220c0a0b0b3
domieee <99103235+domieee@users.noreply.github.com>
2023-01-03 15:06:11 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+// Erstelle eine Variable mit dem Namen carName, und weise ihr den Wert BMW zu.
+// Gib die Variable mit der Konsole aus.
+
+let carName;
+carName = "BMW";
+console.log(carName);
+
+// Erstelle eine Variable mit dem Namen x, und weise ihr den Wert 150 zu. 
+// Gib die Variable mit der Konsole aus.
+
+let x;
+x = 150;
+console.log(x);
+
+// Erstelle eine Variable mit dem Namen y, und weise ihr den Wert 50 zu. 
+// Gib die Variable mit der Konsole aus.
+
+let y;
+y = 150;
+console.log(y);
+
+// Erstelle eine Variable mit dem Namen z, und weise ihr den Wert x+y zu. 
+// Gib die Variable mit der Konsole aus.
+
+let z;
+z = (x + y);
+console.log(z);
+
+// Deklariere in einer einzelnen Zeile drei Variablen mit den folgenden Namen und Werten:
+// let firstName;
+// let lastName;
+// let age;
+
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+
+let firstName = "John", lastName = "Doe", age = 35; 
+
+// Gib die Variablen mit der Konsole aus.
+
+console.log(firstName, lastName, age);
